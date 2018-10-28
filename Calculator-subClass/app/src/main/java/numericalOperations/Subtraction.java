3af9aeb4cf380f7f51f3955908ea6dd8a9798f70
Xatzakos <37630505+Xatzakos@users.noreply.github.com>
2018-10-28 14:33:14 +0200

    FInally ?
    epitelous lew

---
+++ Calculator-subClass/app/src/main/java/numericalOperations/Subtraction.java
@@ -0,0 +1,11 @@
+package numericalOperations;
+
+public class Subtraction
+{
+    //Subtraction function for double values
+    public static Double DoubleValues(String value1,String value2)    {
+        Double result=null;
+        result=Double.parseDouble(value1)-Double.parseDouble(value2);
+        return result;
+    }
+}
