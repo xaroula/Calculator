4bd08362014e631bd3acafe7ad168d81f3a23717
Xatzakos <kxatzakos@gmail.com>
2018-10-30 08:57:37 +0200

    Correct finally
    123

--- app/src/main/java/numericalOperations/Subtraction.java
+++ app/src/main/java/numericalOperations/Subtraction.java
@@ -2,7 +2,7 @@ package numericalOperations;
 
 public class Subtraction {
     //Subtraction function for double values
-    public static Double DoubleValues(String value1,String value2)    {
+    public static Double Subtraction(String value1,String value2)    {
         Double result=null;
         result=Double.parseDouble(value1)-Double.parseDouble(value2);
         return result;
